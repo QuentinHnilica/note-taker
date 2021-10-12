9f2a369a3234e0eccc0b0e21d64de58a651e6f35
unknown <Quentinhnilica@gmail.com>
2021-10-12 16:30:22 -0700

    everything works, horray

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const path = require('path')
 
 const PORT = 3001
-
+let newId = 1
 
 app.use(express.static('public'))
 
@@ -12,19 +12,17 @@ app.use(express.json())
 
 app.use(express.urlencoded({extended: true}))
 
-// GET /notes should return the notes.html file.
+
 
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-// GET * should return the index.html file.
+
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
-// The following API routes should be created:
 
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
 app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf-8', (err, data) => {
         if (err) throw err
@@ -32,15 +30,14 @@ app.get('/api/notes', (req, res) => {
     })
 })
 
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to giveeach note a unique id when it's saved (look into npm packages that could do this for you).
 
 app.post('/api/notes', (req, res) => {
     if (req.body.title && req.body.text){
-        let newId = 1
+        
         const newCard = {title: req.body.title,
         text: req.body.text,
         id: newId}
-        //newId ++
+        newId ++
         fs.readFile('./db/db.json', 'utf-8', (err, data) =>{
             if(err) throw err
             const oldJson = JSON.parse(data)
@@ -48,17 +45,37 @@ app.post('/api/notes', (req, res) => {
             let newJson = JSON.stringify(oldJson)
             fs.writeFile('./db/db.json', newJson, (err) => {
                 if (err) throw err
-                res.status(200)
+                res.status(200).send('Succesfully sent')
             })       
         })
     }
     else{
-        res.status(400)
+        res.status(400).send('negative')
     }
 })
 
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err
+        const oldData = JSON.parse(data)
+        const theId = req.params.id
+        const theRealJson = oldData.filter(note =>{
+            if (note.id != theId){
+                return true
+                
+            }
+            else{
+                return false
+            }
+        })
+        let strJson = JSON.stringify(theRealJson)
+        fs.writeFile('./db/db.json', strJson, (err) => {
+            if (err) throw err
+            res.status(200).send('successfully destroyed')
+        })
+    })
+})
 
 app.listen(PORT, ()  => {
     console.log('Server listening on http://localhost:' + PORT)
 })
-
